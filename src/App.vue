8723d659c490240ca8b63f65f5a70813587cc894
linmumu <1054742101@qq.com>
2022-08-06 09:17:33 +0800

    HTMl 和 CSS 写好了

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,14 @@
 <style lang="scss">
 @import '~@/assets/styles/reset.scss';
 @import '~@/assets/styles/helper.scss';
-body{
+
+body {
   font-family: $font-hei;
   line-height: 1.5;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
+  font-size: 16px
 }
+
 </style>
