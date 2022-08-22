1fee8b93075367ebfb929e372d7d1ef7d5ed77b4
linmumu <1054742101@qq.com>
2022-08-23 00:32:00 +0800

    重新封装 recordListModel.ts

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,18 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+  data: [] as RecordItem[],
   clone(record: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(record));
   },
-  fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+  create() {
+
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  fetch() {
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
+  },
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 
