f6dbdff819751c415fc0d9f936ff54ea34ded05d
linmumu <1054742101@qq.com>
2022-08-27 01:25:09 +0800

    抽离 TagHelper 作为 mixin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -19,24 +19,24 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import {mixins} from 'vue-class-component';
+import {TagHelper} from '@/mixins/TagHelper';
+
+const tagHelper:any = require('@/mixins/TagHelper');
 
 @Component({
   components: {Button},
   computed: {
     tags() {
       return this.$store.state.tagList;
-    }
+    },
   }
 })
-export default class Labels extends Vue {
-  beforeCreate(){
-    this.$store.commit('fetchTags')
+export default class Labels extends mixins(TagHelper) {
+  beforeCreate() {
+    this.$store.commit('fetchTags');
   }
-  createTag() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {return window.alert('标签名不能为空');}
-    this.$store.commit('createTag',name);
-  }
+
 };
 </script>
 
