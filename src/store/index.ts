91ba8d909537a3b9e4b333fa6f259479e04d731d
linmumu <1054742101@qq.com>
2022-08-27 00:39:00 +0800

    Tags.vue 使用Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
+import idCreate from '@/lib/idCreate';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    recordList: [] as RecordItem[]
+    recordList: [] as RecordItem[],
+    tagList: [] as Tag[]
   },
   mutations: {
     fetchRecords(state) {
@@ -22,6 +24,24 @@ const store = new Vuex.Store({
       window.localStorage.setItem('recordList',
         JSON.stringify(state.recordList));
     },
+    fetchTags(state) {
+      return state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+    },
+    createTag(state, name: string) {
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签  名重复了');
+        return 'duplicated';
+      }
+      const id = idCreate().toString();
+      state.tagList.push({id, name: name});
+      store.commit('saveTags');
+      window.alert('添加成功');
+      return 'success';
+    },
+    saveTags(state) {
+      window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
+    }
   }
 });
 
