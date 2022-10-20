20ef3e9e508dd45ae4aab000133846a0e2611f5f
linmumu <1054742101@qq.com>
2022-10-21 03:00:07 +0800

    修改远程仓库地址

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -5,7 +5,7 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@gitee.com:linmumujiayou/pig-bookkeeping.git &&
+git remote add origin git@gitee.com:linmumujiayou/morney-website.git &&
 git push -u -f origin "master"
 
 cd ..
